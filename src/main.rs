d9621842a5b5b93cad24551863b4683ac01e4dfa
BernardoMC <88306132+bcmaia@users.noreply.github.com>
2023-04-27 22:56:14 -0300

    Optimized it and added horizontal display.

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,6 @@
-// mod bit;
-// mod sample;
-// mod pulse;
-// mod encoding;
-
-use std::io::{self};
-mod utils;
-use utils::encoder::Encoder;
+use term_size::dimensions_stdout;
 
 fn main() {
-	// Read user input
-	println!("\n\nBlock Encoding method:  \n0 > No block encoding (default)  \n1 > 4B5B");
-	let mut input = String::new();
-	io::stdin().read_line(&mut input).expect("Failed to read input");
-	let block_encode_index = match input.trim().parse::<i32>() {
-		Ok(x) => x,
-		Err(_) => {println!("ERROR: INVALID INPUT, ASSUMING DEFAULT AS ANSWER.\n\n"); 0}
-	};
-
-	println!("\n\nLine Encoding method:  \n0 > NRZ (default) \n1 > NRZI \n2 > Bipolar-AMI \n3 > Pseudoternário \n4 > Manchester \n5 > Manchester Diferencial");
-	let mut input = String::new();
-	io::stdin().read_line(&mut input).expect("Failed to read input");
-	let line_encode_index = match input.trim().parse::<i32>() {
-		Ok(x) => x,
-		Err(_) => {println!("ERROR: INVALID INPUT, ASSUMING DEFAULT AS ANSWER.\n\n"); 0}
-	};
-
-	println!("\n\nWrite a sequence of zeros and ones: ");
-	let mut input = String::new();
-	io::stdin().read_line(&mut input).expect("Failed to read input");
-
-	// Creating encoding pipeline
-	let encod = Encoder::new(block_encode_index as u8, line_encode_index as u8);
-	let text = String::from(input.trim());
-
-	let signal = encod.encode(&text);
-	println!("\n");
-	Encoder::print(&signal);
-	println!("\n");
+    let (width, height) = dimensions_stdout().unwrap();
+    println!("Terminal size: {} x {}", width, height);
 }
-
-    
